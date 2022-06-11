6513da5fff63fd3f766afc710c711f26f311a914
bhankymyk <bankole123michael@gmail.com>
2022-06-11 16:28:58 +0100

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#nclude "lists.h"
+
+/**
+ * dlistint_len - deduce length of a linked list
+ * @h: pointer to the head of list
+ * Return: number
+ */
+size_t dlistint_len(const dliist_t *h)
+{
+    size_t b;
+
+    for (b = 0; h != NULL; b++)
+            h = h->next;
+
+    return (b)
+}
