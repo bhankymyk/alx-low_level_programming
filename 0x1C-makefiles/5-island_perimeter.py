3baee7b8f1c16f6c2e77131085be3582cf15cc0c
bhankymyk <bankole123michael@gmail.com>
2022-08-20 13:42:00 +0100

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"This scripts returns thr perimeter of the island"
+
+
+def island_perimeter(grid):
+    "Returns perimeter from a described grid"
+    count = 0
+    index = 0
+
+    if not isinstance(grid, list):
+        return 0
+
+        for row in grid:
+            row_len = len(row)
+        row_len += 2
+
+        grid_line = []
+        i = 0
+        while i < row_len:
+            grid_line.append(0)
+            i += 1
+        for row in grid:
+            grid_line.append(0)
+            for item in row:
+                grid_line.append(item)
+            grid_line.append(0)
+            i += 1
+
+            for item in grid_line:
+                if item == 1:
+                    if grid_line[index - row_len] == 0:
+                        count += 1
+                    if grid_line[index - 1] == 0:
+                        count += 1
+                    if grid_line[index + 1] == 0:
+                        count += 1
+                    if grid_line[index + row_len] == 0:
+                        count += 1
+
+                index += 1
+            return count
