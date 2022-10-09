0e78f3f68878dd1bcd3b613700ef2c10f0e403e1
bhankymyk <bankole123michael@gmail.com>
2022-10-09 15:58:36 +0100

    103-exponential.c kernel modification

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,12 +1,18 @@
 #include "search_algos.h"
 
 /**
- * print_array - prints an array of integers
- * @array: pointer to the start of the array
- * @size: size of the array (number of elements)
- *
- * Return: nothing
- */
+  * _binary_search - Searches for a value in a sorted array
+  *                  of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 
 int _binary_search(int *array, size_t left, size_t right, int value)
 {
